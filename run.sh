f0a40ebad4dbd4ffe92babfe29a3fd4609913e61
volyx <volyihin@gmail.com>
2017-06-24 14:53:36 +0300

    change port

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -t egor . && docker run -d -p 3000:80 --rm --name egor-running egor
+docker build -t egor . && docker run -d -p 3000:300 --rm --name egor-running egor
